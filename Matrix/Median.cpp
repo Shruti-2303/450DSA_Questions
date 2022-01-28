05d6766a39bb9e6bebb7a63c63c0e0b05df6011d
Shruti Sharma <sharmas23032001@gmail.com>
2022-01-29 01:53:57 +0530

    more in matrix

---
+++ Matrix/Median.cpp
@@ -0,0 +1,51 @@
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int median(vector<vector<int>> &matrix, int r, int c){
+        vector<int>ans;
+        int temp=0;
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                ans.push_back(matrix[i][j]);
+            }
+        }
+        sort(ans.begin(),ans.end());
+        int n=ans.size();
+        for(int i=0;i<n;i++)
+        {
+            int t=n/2;
+            temp=ans[t];
+        }
+        return temp;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int r, c;
+        cin>>r>>c;
+        vector<vector<int>> matrix(r, vector<int>(c));
+        for(int i = 0; i < r; ++i)
+            for(int j = 0;j < c; ++j)
+                cin>>matrix[i][j];
+        Solution obj;
+        cout<<obj.median(matrix, r, c)<<endl;        
+    }
+    return 0;
+}  // } Driver Code Ends
